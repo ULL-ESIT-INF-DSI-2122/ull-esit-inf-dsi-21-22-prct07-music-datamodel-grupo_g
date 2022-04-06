217e207a3ce67a2744695e7ba02e196c7b89f9b8
Dana Choque <alu0101328348@ull.edu.es>
2022-04-06 20:33:33 +0000

    albums and playlist

--- src/Album.ts
+++ src/Album.ts
@@ -2,7 +2,9 @@ import {Groups} from './Groups';
 import {Artists} from './Artists';
 import {Song} from './Song';
 import {MusicalGenres} from './MusicalGenres';
-
+/**
+ * Album class
+ */
 export class Album {
   constructor(private name: string,
               private by: Artists | Groups,
@@ -16,5 +18,83 @@ export class Album {
 
   setName(name: string): void {
     this.name = name;
+  }
+
+  getBy(): Artists | Groups {
+    return this.by;
+  }
+
+  setBy(artistsGroups: (Artists | Groups)): void {
+    this.by = artistsGroups;
+  }
+
+  getYear(): number {
+    return this.year;
+  }
+
+  setYear(newYear: number): void {
+    this.year = newYear;
+  }
+
+  getGenre(): MusicalGenres[] {
+    return this.genre;
+  }
+
+  setGenre(newGenre: MusicalGenres[]): void {
+    this.genre = newGenre;
+  }
+
+  addGenre(newGenre: MusicalGenres): void {
+    this.genre.push(newGenre);
+  }
+
+  removeGenre(genre: string): void {
+    for (let i = 0; i < this.genre.length; i++) {
+      if (this.genre[i].getName() === genre) {
+        this.genre.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  getSongs(): Song[] {
+    return this.songs;
+  }
+
+  setSongs(newSong: Song[]): void {
+    this.songs = newSong;
+  }
+
+  addSongs(newSong: Song): void {
+    this.songs.push(newSong);
+  }
+
+  removeSongs(songs: string): void {
+    for (let i = 0; i < this.songs.length; i++) {
+      if (this.songs[i].getName() === songs) {
+        this.songs.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  printInfo(): void {
+    console.log('Name: ' + this.name);
+    if (this.by instanceof Artists) {
+      console.log('By artists: ' + this.by.getName());
+    } else if (this.by instanceof Groups) {
+      console.log('By groups: ' + this.by.getName());
+    } else {
+      console.log('Error. Which type is it: by groups or by artists?');
+    }
+    console.log('Year: ' + this.year);
+    console.log('Genres: ');
+    for (let i = 0; i < this.genre.length; i++) {
+      console.log(this.genre[i].getName());
+    }
+    console.log('Songs: ');
+    for (let i = 0; i < this.songs.length; i++) {
+      console.log(this.songs[i].getName());
+    }
   }
 }
--- src/Playlist.ts
+++ src/Playlist.ts
@@ -1,9 +1,132 @@
 import {Song} from './Song';
 import {MusicalGenres} from './MusicalGenres';
-
+/**
+ * Playlist class
+ */
 export class Playlist {
   constructor(private name: string,
               private songs: Song[],
               private duration: string,
               private genres: MusicalGenres[]) {}
+  /**
+   * Getter of the private attribute "name"
+   * @returns {string} Name of the playlist
+   */
+  getName(): string {
+    return this.name;
+  }
+
+  /**
+   * Setter: modify the private attribute
+   * @param {string} newName New name of the playlist
+   */
+  setName(newName: string): void {
+    this.name = newName;
+  }
+
+  /**
+   * Getter of the private attribute "songs"
+   * @returns {Song[]} Songs of playlist
+   */
+  getSongs(): Song[] {
+    return this.songs;
+  }
+
+  /**
+   * Setter: modify the list of songs in the playlist
+   * @param {Song[]} newSongs New list of songs
+   */
+  setSongs(newSongs: Song[]): void {
+    this.songs = newSongs;
+  }
+
+  /**
+   * Adds a song in the list of songs in the playlist
+   * @param {Song} newSong New song
+   */
+  addSong(newSong: Song): void {
+    this.songs.push(newSong);
+  }
+
+  /**
+   * Revomes a song in the list of songs in the playlist
+   * @param {string} songs Song to be removed
+   */
+  removeSongs(songs: string): void {
+    for (let i = 0; i < this.songs.length; i++) {
+      if (this.songs[i].getName() === songs) {
+        this.songs.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  /**
+   * Getter of the private attribute "duration"
+   * @returns {string} Duration of the all songs on the playlist
+   */
+  getDuration(): string {
+    return this.duration;
+  }
+
+  /**
+   * Setter: modify the durations of th all songs on the playlist
+   * @param {string} newDuration New Duration
+   */
+  setDuration(newDuration: string): void {
+    this.duration = newDuration;
+  }
+
+  /**
+   * Getter of genres of the playlist
+   * @returns {MusicalGenres[]} Current genres of the playlist
+   */
+  getGenres(): MusicalGenres[] {
+    return this.genres;
+  }
+
+  /**
+   * Setter: modify de current genres
+   * @param {MusicalGenres[]} newGenres New genres of the playlist
+   */
+  setGenres(newGenres: MusicalGenres[]): void {
+    this.genres = newGenres;
+  }
+
+  /**
+   * Adds a genre in the list of genres in the playlist
+   * @param {MusicalGenres} newGenre New genre
+   */
+  addGenres(newGenre: MusicalGenres): void {
+    this.genres.push(newGenre);
+  }
+
+  /**
+   * Removes a genre in the list of genres in the playlist
+   * @param {string} genre Genre to be removed
+   */
+  removeGenres(genre: string): void {
+    for (let i = 0; i < this.genres.length; i++) {
+      if (this.genres[i].getName() === genre) {
+        this.genres.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  /**
+   * Prints playlist information
+   */
+  printInfo(): void {
+    console.log('Name:' + this.name);
+    console.log('Songs: ');
+    for (let i = 0; i < this.songs.length; i++) {
+      console.log(this.songs[i].getName());
+    }
+    console.log('Duration: ' + this.duration);
+    console.log('Genres: ');
+    for (let i = 0; i < this.genres.length; i++) {
+      console.log(this.genres[i].getName());
+    }
+  }
 }
