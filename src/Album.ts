38f2c9c53bb459204d7a3aadd8445235e352badb
Dana Choque <alu0101328348@ull.edu.es>
2022-04-08 16:49:38 +0000

    album comments

--- src/Album.ts
+++ src/Album.ts
@@ -11,43 +11,82 @@ export class Album {
               private year: number,
               private genre: MusicalGenres[],
               private songs: Song[]) {}
-
+  /**
+   * Getter of the private attribute "name"
+   * @returns {string} Name of the album
+   */
   getName(): string {
     return this.name;
   }
 
+  /**
+   * Setter: modify the private attribute
+   * @param {string} name New name of the album
+   */
   setName(name: string): void {
     this.name = name;
   }
 
+  /**
+   * Getter that indicates if the album was published by artist or by group
+   * @returns {Artists | Groups} Return the type of author
+   */
   getBy(): Artists | Groups {
     return this.by;
   }
 
+  /**
+   * Setter: modify the type of author published the album
+   * @param {Artists | Groups} artistsGroups Object of type Artist or Group
+   */
   setBy(artistsGroups: (Artists | Groups)): void {
     this.by = artistsGroups;
   }
 
+  /**
+   * Getter of the year of the album
+   * @returns {number} Year of the album
+   */
   getYear(): number {
     return this.year;
   }
 
+  /**
+   * Setter: modify the private attribute
+   * @param {number} newYear New year of the album
+   */
   setYear(newYear: number): void {
     this.year = newYear;
   }
 
+  /**
+   * Getter of genres of the album
+   * @returns {MusicalGenres[]}
+   */
   getGenre(): MusicalGenres[] {
     return this.genre;
   }
 
+  /**
+   * Setter: modify the genres of the album
+   * @param {MusicalGenres[]} newGenre New genre of the album
+   */
   setGenre(newGenre: MusicalGenres[]): void {
     this.genre = newGenre;
   }
 
+  /**
+   * Adds a genre in the album
+   * @param {MusicalGenres} newGenre New genre
+   */
   addGenre(newGenre: MusicalGenres): void {
     this.genre.push(newGenre);
   }
 
+  /**
+   * Removes a genre in the album
+   * @param {string} genre Genre to be removed
+   */
   removeGenre(genre: string): void {
     for (let i = 0; i < this.genre.length; i++) {
       if (this.genre[i].getName() === genre) {
@@ -57,18 +96,34 @@ export class Album {
     }
   }
 
+  /**
+   * Getter of the list of songs of the album
+   * @returns {Song[]} Current songs list
+   */
   getSongs(): Song[] {
     return this.songs;
   }
 
+  /**
+   * Setter: modify the list songs of the album
+   * @param {Song[]} newSong New songs list
+   */
   setSongs(newSong: Song[]): void {
     this.songs = newSong;
   }
 
+  /**
+   * Adds a song in the list of songs of the album
+   * @param {Song} newSong New song
+   */
   addSongs(newSong: Song): void {
     this.songs.push(newSong);
   }
 
+  /**
+   * Revomes a song in the list of songs of the album
+   * @param {string} songs Song to be removed
+   */
   removeSongs(songs: string): void {
     for (let i = 0; i < this.songs.length; i++) {
       if (this.songs[i].getName() === songs) {
@@ -78,6 +133,9 @@ export class Album {
     }
   }
 
+  /**
+   * Prints album information
+   */
   printInfo(): void {
     console.log('Name: ' + this.name);
     if (this.by instanceof Artists) {
@@ -89,12 +147,12 @@ export class Album {
     }
     console.log('Year: ' + this.year);
     console.log('Genres: ');
-    for (const value of this.genre) {
-      console.log(value.getName());
+    for (let i = 0; i < this.genre.length; i++) {
+      console.log(this.genre[i].getName());
     }
     console.log('Songs: ');
-    for (const value of this.songs) {
-      console.log(value.getName());
+    for (let i = 0; i < this.songs.length; i++) {
+      console.log(this.songs[i].getName());
     }
   }
 }
