948b2a0f2c97302f5bd23815cc9adce46db2b493
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2022-04-10 12:21:54 +0000

    Update Manager Class

---
+++ src/Manager.ts
@@ -0,0 +1,58 @@
+
+import {Playlist} from './Playlist';
+
+export class Manager {
+  constructor(private playlists: Playlist[]) {}
+
+  getPlaylists(): Playlist[] {
+    return this.playlists;
+  }
+
+  showPlaylists(): void {
+    for (const playlist of this.playlists) {
+      playlist.printInfo();
+    }
+  }
+
+  navegatePlaylists(order: string): void {
+    this.showPlaylists();
+    for (let i = 0; i < this.playlists.length; i++) {
+      if (order === 'name asc') {
+        console.log(this.playlists[i].getSongs().sort((a, b) => a.getName().localeCompare(b.getName())));
+      } else if (order === 'name desc') {
+        console.log(this.playlists[i].getSongs().sort((a, b) => b.getName().localeCompare(a.getName())));
+      } else if (order === 'artist asc') {
+        console.log(this.playlists[i].getSongs().sort((a, b) => a.getArtist().getName().localeCompare(b.getArtist().getName())));
+      } else if (order === 'artist desc') {
+        console.log(this.playlists[i].getSongs().sort((a, b) => b.getArtist().getName().localeCompare(a.getArtist().getName())));
+      } else if (order === 'duration asc') {
+        console.log(this.playlists[i].getSongs().sort((a, b) => a.getDuration().localeCompare(b.getDuration())));
+      } else if (order === 'duration desc') {
+        console.log(this.playlists[i].getSongs().sort((a, b) => b.getDuration().localeCompare(a.getDuration())));
+      } else if (order === 'genre asc') {
+        console.log(this.playlists[i].getSongs().sort((a, b) => a.getGenre()[i].getName().localeCompare(b.getGenre()[i].getName())));
+      } else if (order === 'genre desc') {
+        console.log(this.playlists[i].getSongs().sort((a, b) => b.getGenre()[i].getName().localeCompare(a.getGenre()[i].getName())));
+      } else if (order === 'num rep asc') {
+        console.log(this.playlists[i].getSongs().sort((a, b) => a.getNumRep() - b.getNumRep()));
+      } else if (order === 'num rep desc') {
+        console.log(this.playlists[i].getSongs().sort((a, b) => b.getNumRep() - a.getNumRep()));
+      } else {
+        console.log(this.playlists[i].getSongs().sort((a, b) => a.getName().localeCompare(b.getName())));
+      }
+    }
+  }
+
+  createPlaylist(name: string, type: string): void {
+    if (type === 'existing') {
+      if (this.playlists.find((playlist) => playlist.getName() === name)) {
+        console.log('Playlist already exists');
+      } else {
+        // this.playlists.push(new Playlist(name));
+      }
+    }
+    if (type === 'personalized') {
+      // this.playlists.push(new Playlist(name));
+    }
+  }
+}
